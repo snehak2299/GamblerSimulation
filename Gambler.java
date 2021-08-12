bfae23a2cd1836ac8404c887aa666736d7b26cd5
snehak2299 <skanase2299@gmail.com>
2021-08-12 21:44:58 +0530

    [sneha]:stop playing if gambler win or lose 50%

--- Gambler.java
+++ Gambler.java
@@ -6,15 +6,23 @@ public class Gambler {
 		int everyDay = 100;
 		final int betPerGame = 1;
 		int wins = 0;
-		if(Math.random()<0.5) {
-			wins+=1;
-			everyDay=everyDay+betPerGame;
-			System.out.println(everyDay);
+		int totalStake = everyDay;
+		double resignAmount = 0.5;
+		while(everyDay>(int)(totalStake*resignAmount) && everyDay<=(int)(totalStake+(totalStake*resignAmount))) {
+			if(Math.random()<0.5) {
+				wins+=1;
+				everyDay+=betPerGame;
+			}
+			else {
+				wins-=1;
+				everyDay-=betPerGame;
+			}
+		}
+		if(everyDay==(int)(totalStake*resignAmount)) {
+			System.out.println("gambler stop playing due to lossing");
 		}
 		else {
-			wins-=1;
-			everyDay=everyDay-betPerGame;
-			System.out.println(everyDay);
+			System.out.println("gambler stop playing due to winning");
 		}
 
 	}
