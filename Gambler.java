879a44fd96fde46a51e5673dea63b09117cef98b
snehak2299 <skanase2299@gmail.com>
2021-08-12 21:09:40 +0530

    [sneha]:added either win or loss

--- Gambler.java
+++ Gambler.java
@@ -5,7 +5,17 @@ public class Gambler {
 	public static void main(String[] args) {
 		int everyDay = 100;
 		final int betPerGame = 1;
-		
+		int wins = 0;
+		if(Math.random()<0.5) {
+			wins+=1;
+			everyDay=everyDay+betPerGame;
+			System.out.println(everyDay);
+		}
+		else {
+			wins-=1;
+			everyDay=everyDay-betPerGame;
+			System.out.println(everyDay);
+		}
 
 	}
 
